e845295f0ef5b1e2460df431425299a7ec3bbc8b
flaviojcf <flaviojcostafilho@gmail.com>
2026-06-07 19:37:48 -0300

    feat: add otel datadog observability

--- src/Fcs.Campaign.WebApi/Observability/ObservabilityOptions.cs
+++ src/Fcs.Campaign.WebApi/Observability/ObservabilityOptions.cs
@@ -9,4 +9,10 @@ public sealed class ObservabilityOptions
     public const string SectionName = "Observability";
 
     public string ServiceName { get; set; } = "fcs.Campaign";
+
+    public bool EnableOtlpExporter { get; set; }
+
+    public string OtlpEndpoint { get; set; } = string.Empty;
+
+    public string OtlpAuthHeader { get; set; } = string.Empty;
 }
